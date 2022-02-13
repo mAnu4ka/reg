acbe4c88c725eba1e1901e80045d907beb0b2824
mAnu4ka <94441221+mAnu4ka@users.noreply.github.com>
2022-02-13 19:43:02 +0500

    Add files via upload

--- auth.js
+++ auth.js
@@ -7,6 +7,16 @@ let routes = {
 
 // Регистрация пользователя
 let form = document.querySelector('form')
+let input = document.querySelectorAll('input')
+const Erroridk = () => {
+    for (let item of input){
+        item.classList.add('bad')
+        let p = document.createElement("p");
+        item.after(p)
+        p.classList.add('dont_hit')
+        p.innerText = 'Ведите все правельно'
+    }
+}
 console.log(api + routes.login);
 form.onsubmit = () => {
     event.preventDefault()
@@ -22,9 +32,12 @@ form.onsubmit = () => {
         .then(res => {
             if (res.status == 200 || res.status == 201) {
                 let user = JSON.parse(localStorage.user)
-                console.log(user);
+                console.log(user.password);
                 if (user.email == obj.email && user.password == obj.password) {
                     window.location.href = "./index3.html"
+                    return
+                } else {
+                    Erroridk()
                 }
                 return
             }
